7e32f0663b1d32e42d21e3404920ed16d2b92ef5
Student <example@example.com>
2019-12-16 09:07:48 -0800

    follow along with lesson

---
+++ lib/project.rb
@@ -0,0 +1,37 @@
+class Album
+  attr_reader :id, :name
+
+  @@albums = {}
+  @@total_rows = 0
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@albums.values()
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def update(name)
+    @name = name
+  end
+
+def delete
+  @@albums.delete(self.id)
+  end
+end
