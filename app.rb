c99742f7668ab7d87661d8f6d39a9f271e66e07c
Lara Bjork <lara.m.bjork@gmail.com>
2019-12-17 19:48:29 -0800

    add CRUD functionality for songs--copy and paste from homework

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/reloader')
 require('./lib/project')
 require('pry')
 also_reload('lib/**/*.rb')
+require('./lib/song')
 
 get('/test/') do
    @something = "this is a variable"
@@ -71,10 +72,6 @@ patch('/albums/:id') do
   erb(:albums)
 end
 
-
-
-
-
 delete('/albums/:id') do
   @album = Album.find(params[:id].to_i())
   @album.delete()
@@ -88,4 +85,34 @@ end
 
 get('/albums/:id/edit') do
   "This will take us to a page with a form for updating an album with an ID of #{params[:id]}."
+end
+
+# Get the detail for a specific song such as lyrics and songwriters.
+get('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+# Post a new song. After the song is added, Sinatra will route to the view for the album the song belongs to.
+post('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  erb(:album)
+end
+
+# Edit a song and then route back to the album view.
+patch('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name], @album.id)
+  erb(:album)
+end
+
+# Delete a song and then route back to the album view.
+delete('/albums/:id/songs/:song_id') do
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
 end
